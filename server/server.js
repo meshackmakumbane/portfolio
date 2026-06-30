1c88faa7735c41c66e32d69fcc4cbef1f3979f76
Meshack Makumbane <meshackmakumbane@gmai.com>
2026-06-30 23:35:10 +0200

    implement new Backend URL

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(cors({
-    origin: "https://makumbaneportfolio.netlify.app/"
+    origin: "https://makumbaneportfolio.netlify.app"
 }));
 
 //Health check
